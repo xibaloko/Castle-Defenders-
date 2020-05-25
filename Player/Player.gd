fec5ab1633d2a09b3390466d5704cc4798892e2a
Ulisses Inacio <ulissesmilitao@hotmail.com>
2020-05-24 21:31:27 -0300

    Implementando o chão e as animações do personagem principal

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,20 +1,27 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const GRAVITY = 20
-const SPEED = 20
-const JUMP_HEIGHT = -40
+const GRAVITY = 15
+const SPEED = 50
+const JUMP_HEIGHT = -300
 
 var motion = Vector2()
 
 func _physics_process(delta):
 	
+	motion.y += GRAVITY
+	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		$AnimatedSprite.play("Running")
+		$AnimatedSprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = SPEED
+		motion.x = -SPEED
+		$AnimatedSprite.play("Running")
+		$AnimatedSprite.flip_h = true
 	else:
 		motion.x = 0
+		$AnimatedSprite.play("Idle")
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
