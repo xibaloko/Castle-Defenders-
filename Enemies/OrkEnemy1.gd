92ba326f70d68707fe27bee1cb548b33b12264ad
Ulisses Inacio <ulissesmilitao@hotmail.com>
2020-05-26 22:09:22 -0300

    Adicionando mecanica de ataque e destruicao do inimigo

--- Enemies/OrkEnemy1.gd
+++ Enemies/OrkEnemy1.gd
@@ -1,13 +1,27 @@
 extends KinematicBody2D
 
+var dead = false;
 var speed = 50
 
 func _ready():
 	$AnimatedSprite.flip_h = true
-	$AnimatedSprite.play("Running")
 	
 func _process(delta):
+	if dead == false:
+		$AnimatedSprite.play("Running")
+	
 	var castle = get_tree().get_root().get_node("Main").get_node("Castle")
 	var dir = (castle.global_position - global_position).normalized()
 	
 	move_and_collide(dir*speed*delta)
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("Sword"):
+		dead = true
+		$AnimatedSprite.play("Dying")
+
+
+func _on_AnimatedSprite_animation_finished():
+	if $AnimatedSprite.animation == "Dying":
+		queue_free()
--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,7 @@ const JUMP_HEIGHT = -300
 
 var life = 50
 var energy = 50
+var isAttacking = false
 
 var motion = Vector2()
 
@@ -19,20 +20,31 @@ func _ready():
 func _physics_process(delta):
 	motion.y += GRAVITY
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") && isAttacking == false:
 		motion.x = SPEED
 		$AnimatedSprite.play("Running")
 		$AnimatedSprite.flip_h = false
-	elif Input.is_action_pressed("ui_left"):
+		get_node("AttackArea").set_scale(Vector2(1, 1))
+	elif Input.is_action_pressed("ui_left") && isAttacking == false:
 		motion.x = -SPEED
 		$AnimatedSprite.play("Running")
 		$AnimatedSprite.flip_h = true
+		get_node("AttackArea").set_scale(Vector2(-1, 1))
 	else:
 		motion.x = 0
-		$AnimatedSprite.play("Idle")
-	
+		if isAttacking == false:
+			$AnimatedSprite.play("Idle")
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+	if Input.is_action_just_pressed("Attack"):
+		$AnimatedSprite.play("Attacking")
+		isAttacking = true
+		$AttackArea/CollisionShape2D.disabled = false
 	
 	motion = move_and_slide(motion, UP)
+
+func _on_AnimatedSprite_animation_finished():
+	if $AnimatedSprite.animation == "Attacking":
+		$AttackArea/CollisionShape2D.disabled = true
+		isAttacking = false
